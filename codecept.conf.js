436d8a16412ae871d6bf3a0ba67e32e0aafc43bc
001anj <33808621+001anj@users.noreply.github.com>
2020-02-11 00:07:04 +0100

    Create codecept.conf.js

---
+++ codecept.conf.js
@@ -0,0 +1,34 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'projectCharly',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    allure: {
+    	outputDir: './output'
+
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
